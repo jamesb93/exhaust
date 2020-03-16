94a3383388397fbc24ec22f41f374fb443d70adb
James Bradbury <jamesbradbury93@gmail.com>
2020-03-16 19:08:52 +0000

    add jsonutils

--- jsonutils.py
+++ jsonutils.py
@@ -1,4 +1,7 @@
-def write_json(json_file_path: str, in_dict: dict):
+import json as rj
+
+
+def write_json(json_file_path: str, in_dict: dict, indent: int = 4):
     """Takes a dictionary and writes it to JSON file"""
     with open(json_file_path, "w+") as fp:
-        rj.dump(in_dict, fp, indent=4)
+        rj.dump(in_dict, fp, indent=indent)
